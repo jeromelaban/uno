59a8c3cffa7872e206ba6081f6f64003e9f8f1de
Jerome Laban <jerome.laban@nventive.com>
2020-09-10 11:22:01 -0400

    wip

---
+++ src/Uno.UI.Tests/Windows_UI_Xaml_Data/xBindTests/Given_xBind_Binding_Poco.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uno.UI.Tests.Windows_UI_Xaml_Data.xBindTests.Controls;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.UI.Tests.Windows_UI_Xaml_Data.xBindTests
+{
+	[TestClass]
+	public class Given_xBind_Binding_Poco
+	{
+		[TestMethod]
+		public void When_Basic_PocoBinding()
+		{
+
+		}
+	}
+}
