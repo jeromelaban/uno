74de9100202ddecffddb634fbe0943202fb236bc
Jerome Laban <jerome.laban@nventive.com>
2020-11-19 08:24:54 -0500

    ci: Test failure

--- src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/ButtonTests/Button_Tests.cs
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/ButtonTests/Button_Tests.cs
@@ -15,6 +15,13 @@ namespace SamplesApp.UITests.Windows_UI_Xaml_Controls.ButtonTests
 		[AutoRetry]
 		public void Validate_UseUWPDefaultStyles()
 		{
+			Assert.Fail();
+		}
+
+		[Test]
+		[AutoRetry]
+		public void Validate_UseUWPDefaultStyles()
+		{
 			Run("UITests.Shared.Windows_UI_Xaml_Controls.Button.Button_UseUWPDefaultStyles");
 
 			_app.WaitForText("ResultsTextBlock", "Native view found");
