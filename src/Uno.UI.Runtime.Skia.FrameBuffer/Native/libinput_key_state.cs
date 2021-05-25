36d2fa06d2543eb9790c6d7629b9fb28c9de01d8
Jerome Laban <jerome.laban@nventive.com>
2021-05-25 11:18:46 -0400

    update

--- src/Uno.UI.Runtime.Skia.FrameBuffer/Native/libinput_key_state.cs
+++ src/Uno.UI.Runtime.Skia.FrameBuffer/Native/libinput_key_state.cs
@@ -1,4 +1,7 @@
-﻿namespace Uno.UI.Runtime.Skia.Native
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Uno.UI.Runtime.Skia.Native
 {
 	enum libinput_key_state
 	{
